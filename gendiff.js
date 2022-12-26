af0f308462ab2af2c32c9ca1d813230615562ce8
Noskov Vladimir <burger_ok@mail.ru>
2022-12-27 00:02:30 +0300

    added arguments & format option (step 2)

--- gendiff.js
+++ gendiff.js
@@ -6,4 +6,6 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
